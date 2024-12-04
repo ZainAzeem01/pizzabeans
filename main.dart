2f8266b383973f1588747ec85f392488e9239b78
ZainAzeem01 <129539714+ZainAzeem01@users.noreply.github.com>
2024-12-04 22:01:57 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pizzabeans/pages/addtocart.dart';
+import 'package:pizzabeans/pages/auth.dart';
+import 'package:pizzabeans/pages/signup.dart';
+import 'firebase_options.dart';
+import 'pages/login.dart';
+import 'pages/product.dart';
+import 'pages/detail.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      //home: AddToCartApp(),
+      home: const AuthPage(),
+    );
+  }
+}
+
